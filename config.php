ac57da98984510d396c8dacbcdeaf311ea545499
calonso-conabio <calonso@conabio.gob.mx>
2014-08-20 12:47:36 -0500

    Delete config.php

--- config.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$conexion = @mysql_connect("localhost", "root", "") or die ("Fallo en el establecimiento de la conexión");
-mysql_select_db('autocompletar') or die("Error en la selección de la base de datos");
-mysql_query("SET NAMES 'utf8'");
-?> 
