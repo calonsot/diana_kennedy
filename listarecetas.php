b0ca7c902fff030e1fd559f70a7619e236298c8a
iiibella <israel.martinez@gmail.com>
2014-09-02 12:06:09 -0500

    Cambios 2Sep2014
    - Muestra 5 recetas mas
    - Muestra libro en resultado de recetas
    - Base de datos con 4 libros
    - Cambio menu desplegable

--- listarecetas.php
+++ listarecetas.php
@@ -1,152 +1,66 @@
-<!DOCTYPE HTML>
+<?php include('menu.php') ?>
 <?php
-	if(!session_id())session_start();
-	$sessionId = session_id();
-
-	header( 'Content-type: text/html; charset=iso-8859-1' );
-	require('config.php');
 
 	$filtro = $_GET["filtro"];			
 	$sql = "SELECT URL, recetanombre, publico FROM diannakennedy WHERE ".$filtro." ORDER BY recetanombre";		
-	//Conexión a la base de datos 
+	//ConexiÃ³n a la base de datos 
 	$con = $conexion; 	
 
 	//Sentencia sql (sin limit) 
 	$_pagi_sql = $sql; 
 
-	//cantidad de resultados por página (opcional, por defecto 20) 
+	//cantidad de resultados por pÃ¡gina (opcional, por defecto 20) 
 	$_pagi_cuantos = 10; 
 
-	//cantidad de enlaces que se mostrarán como máximo en la barra de navegación
+	//cantidad de enlaces que se mostrarÃ¡n como mÃ¡ximo en la barra de navegaciÃ³n
 	$_pagi_nav_num_enlaces = 10;
 	 
 	//Decidimos si queremos que se muesten los errores de mysql
-	$_pagi_mostrar_errores = true;//recomendado true sólo en tiempo de desarrollo.
+	$_pagi_mostrar_errores = true;//recomendado true sÃ³lo en tiempo de desarrollo.
 	 
 	//Si tenemos una consulta compleja que hace que el Paginator no funcione correctamente, 
 	//realizamos el conteo alternativo.
 	$_pagi_conteo_alternativo = true;//recomendado false.
 	 
-	//Supongamos que sólo nos interesa propagar estas dos variables
-	$_pagi_propagar = array("URL","recetanombre","publico");//No importa si son POST o GET
+	//Supongamos que sÃ³lo nos interesa propagar estas dos variables
+	$_pagi_propagar = array("URL","recetanombre", "publico");//No importa si son POST o GET
 	 
-	//Definimos qué estilo CSS se utilizará para los enlaces de paginación.
+	//Definimos quÃ© estilo CSS se utilizarÃ¡ para los enlaces de paginaciÃ³n.
 	//El estilo debe estar definido previamente
 	$_pagi_nav_estilo = "paginacion";
 
-	//definimos qué irá en el enlace a la página anterior
-	$_pagi_nav_anterior = "&laquo; Anterior"; // &lt;";// podría ir un tag <img> o lo que sea
+	//definimos quÃ© irÃ¡ en el enlace a la pÃ¡gina anterior
+	$_pagi_nav_anterior = "&laquo; Anterior"; // &lt;";// podrÃ­a ir un tag <img> o lo que sea
 	 
-	//definimos qué irá en el enlace a la página siguiente
-	$_pagi_nav_siguiente = "Siguiente &raquo;"; //" &gt;";// podría ir un tag <img> o lo que sea
+	//definimos quÃ© irÃ¡ en el enlace a la pÃ¡gina siguiente
+	$_pagi_nav_siguiente = "Siguiente &raquo;"; //" &gt;";// podrÃ­a ir un tag <img> o lo que sea
 
 	$_pagi_htaccess = "";
 
-	//Incluimos el script de paginación. Éste ya ejecuta la consulta automáticamente 
+	//Incluimos el script de paginaciÃ³n. Ã‰ste ya ejecuta la consulta automÃ¡ticamente 
 	include("paginator.inc.php"); 
 ?>
-<html>
-	<head>
-		<title>Diana Kennedy | Biodiversidad Mexicana | Conabio</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		
-        
-        <script src="js/jquery-1.9.1.js"></script>
-		<script src="js/menu.js"></script>
-		<script type="text/javascript">
-/* <![CDATA[ */
-	( function($) {
-		$.fn.responsiveVideo = function() {
-			// Add CSS to head
-			$( 'head' ).append( '<style type="text/css">.responsive-video-wrapper{width:100%;position:relative;padding:0}.responsive-video-wrapper iframe,.responsive-video-wrapper object,.responsive-video-wrapper embed{position:absolute;top:0;left:0;width:100%;height:100%}</style>' );
-			// Gather all videos
-			var $all_videos = $(this).find( 'iframe[src*="player.vimeo.com"], iframe[src*="youtube.com"], iframe[src*="youtube-nocookie.com"], iframe[src*="dailymotion.com"], iframe[src*="kickstarter.com"][src*="video.html"], object, embed' );
-			// Cycle through each video and add wrapper div with appropriate aspect ratio if required
-			$all_videos.not( 'object object' ).each( function() {
-				var $video = $(this);
-				if ( $video.parents( 'object' ).length )
-					return;
-				if ( ! $video.prop( 'id' ) )
-					$video.attr( 'id', 'rvw' + Math.floor( Math.random() * 999999 ) );
-				$video
-					.wrap( '<div class="responsive-video-wrapper" style="padding-top: ' + ( $video.attr( 'height' ) / $video.attr( 'width' ) * 100 ) + '%" />' )
-					.removeAttr( 'height' )
-					.removeAttr( 'width' )
-					} );
-		};
-	} )(jQuery);
-
-		$(document).ready(function() {
-			$( 'body' ).responsiveVideo();
-		});
-		</script>
-
-			
-		
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-	</head>
-	<body>
-
-		<!-- Header -->
-			<div id="header">
-						
-				<!-- Logo -->
-                    
-              <div id="logo">
-                <img src="images/logo.png" usemap="#Map">
-                <map name="Map">
-                  <area shape="rect" coords="36,5,357,75" href="index.html">
-                  <area shape="rect" coords="674,3,762,73" href="http://www.conabio.gob.mx" target="_blank">
-                </map>
-              </div>
-				
-				<!-- Nav -->
-					<nav id="nav">
-						<ul>
-							<li ><a href="index.php">Inicio</a></li>
-							<li><a href="qs_dk.php">¿Quién es DK?</a></li>
-							<li><a href="quinta.php">Quinta</a></li>
-							<li><a href="proyecto.php">Proyecto</a></li>
-							<li class="current"><a href="recursos.php">Recursos</a></li>
-							<li> <?php include('menu.php') ?></li>
-						</ul>
-			  		</nav>
-
-	</div>
 			
 		<!-- Main -->
 			<section class="wrapper style1">
 				<div class="container">
                 <header>
 						<h4 align="center"><?php 
-											/*if ($_pagi_totalReg <=5)
+										      /*if ($_pagi_totalReg <=5)
 												echo " <span class=fuente8><b>Se encontraron $_pagi_totalReg recetas.</b></span>";
 											else
-						                    	echo $_pagi_info; */
+						                    	echo $_pagi_info;*/ 
 						                  ?></h4>
 				</header>
-				<br>
-				<br>
+		                <br>
+                                <br>
 
 				<?php 					
 					echo "<div class='micaja'>";
 
-					//Leemos y escribimos los registros de la página actual 
+					//Leemos y escribimos los registros de la pÃ¡gina actual 
 					$i = 1;
-					$bandera = true;
+				    $bandera = true;
 					while($row = mysql_fetch_array($_pagi_result))
 					{ 							
 						if ($bandera == true)
@@ -155,9 +69,9 @@
 							echo "<div class='izq'>";
 							echo "<div class='micontenido'>";
 							$row['recetanombre']=htmlentities($row['recetanombre']);
-							echo "<a href='muestrareceta.php?urlreceta=".$row['URL']."&nombrereceta=".$row['recetanombre']."'><img src='images/thumb".$i.".jpg' width='46' height='46' alt='' />&nbsp;&nbsp;&nbsp;".$row['recetanombre']."</a>";
+							echo "<a href='muestrareceta.php?urlreceta=".$row['URL']."&nombrereceta=".$row['recetanombre']."&filtro=".$filtro."'><img src='images/thumb".$i.".jpg' width='46' height='46' alt='' />&nbsp;&nbsp;&nbsp;".$row['recetanombre']."</a>";
 							if ($row['publico']==1)
-						    	echo "<p>&nbsp;&nbsp;&nbsp;Tal vez una breve descripción... </p><br><br>";
+						    	echo "<p>&nbsp;&nbsp;&nbsp;Tal vez una breve descripciï¿½n... </p><br><br>";
 							else
 								echo "<p>&nbsp;&nbsp;&nbsp;No esta disponible esta receta </p><br><br>";
 						    echo "</div>";
@@ -169,15 +83,15 @@
 							echo "<div class='der'>";
 							echo "<div class='micontenido'>";
 							$row['recetanombre']=htmlentities($row['recetanombre']);
-							echo "<a href='muestrareceta.php?urlreceta=".$row['URL']."&nombrereceta=".$row['recetanombre']."'><img src='images/thumb".$i.".jpg' width='46' height='46' alt='' />&nbsp;&nbsp;&nbsp;".$row['recetanombre']."</a>";
+							echo "<a href='muestrareceta.php?urlreceta=".$row['URL']."&nombrereceta=".$row['recetanombre']."&filtro=".$filtro."'><img src='images/thumb".$i.".jpg' width='46' height='46' alt='' />&nbsp;&nbsp;&nbsp;".$row['recetanombre']."</a>";
 						    if ($row['publico']==1)
-						    	echo "<p>&nbsp;&nbsp;&nbsp;Tal vez una breve descripción... </p><br><br>";
+						    	echo "<p>&nbsp;&nbsp;&nbsp;Tal vez una breve descripciï¿½n... </p><br><br>";
 							else
 								echo "<p>&nbsp;&nbsp;&nbsp;No esta disponible esta receta </p><br><br>";
 						    echo "</div>";
 						    echo "</div>";	
-						}						
-
+						}
+		
 					    $i += 1;
 					    if ($i == 4)
 					    	$i = 1;
@@ -185,31 +99,11 @@
 
 					echo "</div>";
 
-					//Incluimos la barra de navegación 
+					//Incluimos la barra de navegaciÃ³n 
 					echo"<center><p>".$_pagi_navegacion."</p></center>";
 					//echo"<p>Mostrando Registrados ".$_pagi_info."</p>";
 				?>                
                
 			</section>			
 
-		<!-- Footer -->
-		<div id="footer">
-        
-		<!-- Copyright -->
-					<div class="copyright">
-					&copy; 2014 Comisión Nacional para el Conocimiento y Uso de la Biodiversidad (CONABIO)
-                    </div>        
-        
-        <!-- Icons -->
-					<ul class="icons">
-						<li><a href="http://www.facebook.com/pages/Comision-Nacional-para-el-Conocimiento-y-uso-de-la-Biodiversidad/208239404119?v=wall" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
-                      <li><a href="http://twitter.com/conabio" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-						<li><a href="http://www.youtube.com/biodiversidadmexico" class="icon fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
-						<li><a href="https://soundcloud.com/conabio" class="icon fa-soundcloud" target="_blank"><span class="label">SoundCloud</span></a></li>
-					</ul>
-
-		
-			</div>
-
-	</body>
-</html>
+		<?php include('footer.php') ?>
--- muestrareceta.php
+++ muestrareceta.php
@@ -1,140 +1,56 @@
-<!DOCTYPE HTML>
-<?php
-	if(!session_id())session_start();
-	$sessionId = session_id();
-
-	header( 'Content-type: text/html; charset=iso-8859-1' );
-	require('config.php');
-
-	$URL = $_GET["urlreceta"];
-	$nombrereceta = $_GET["nombrereceta"];	
+<?php 
+include('menu.php');
+include('config.php');
+$URL = $_GET["urlreceta"];
+$nombrereceta = $_GET["nombrereceta"];	
+$filtro = $_GET["filtro"];
 ?>
-<html>
-	<head>
-		<title>Diana Kennedy | Biodiversidad Mexicana | Conabio</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-		<meta name="description" content="" />
-		<meta name="keywords" content="" />
-		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
-		<script src="js/jquery.min.js"></script>
-		<script src="js/jquery.dropotron.min.js"></script>
-		<script src="js/skel.min.js"></script>
-		<script src="js/skel-layers.min.js"></script>
-		<script src="js/init.js"></script>
-		
-        
-        <script src="js/jquery-1.9.1.js"></script>
-		<script src="js/menu.js"></script>
-		<script type="text/javascript">
-/* <![CDATA[ */
-	( function($) {
-		$.fn.responsiveVideo = function() {
-			// Add CSS to head
-			$( 'head' ).append( '<style type="text/css">.responsive-video-wrapper{width:100%;position:relative;padding:0}.responsive-video-wrapper iframe,.responsive-video-wrapper object,.responsive-video-wrapper embed{position:absolute;top:0;left:0;width:100%;height:100%}</style>' );
-			// Gather all videos
-			var $all_videos = $(this).find( 'iframe[src*="player.vimeo.com"], iframe[src*="youtube.com"], iframe[src*="youtube-nocookie.com"], iframe[src*="dailymotion.com"], iframe[src*="kickstarter.com"][src*="video.html"], object, embed' );
-			// Cycle through each video and add wrapper div with appropriate aspect ratio if required
-			$all_videos.not( 'object object' ).each( function() {
-				var $video = $(this);
-				if ( $video.parents( 'object' ).length )
-					return;
-				if ( ! $video.prop( 'id' ) )
-					$video.attr( 'id', 'rvw' + Math.floor( Math.random() * 999999 ) );
-				$video
-					.wrap( '<div class="responsive-video-wrapper" style="padding-top: ' + ( $video.attr( 'height' ) / $video.attr( 'width' ) * 100 ) + '%" />' )
-					.removeAttr( 'height' )
-					.removeAttr( 'width' )
-					} );
-		};
-	} )(jQuery);
-
-		$(document).ready(function() {
-			$( 'body' ).responsiveVideo();
-		});
-		</script>
-		
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-	</head>
-	<body>
-
-		<!-- Header -->
-			<div id="header">
-						
-				<!-- Logo -->
-                    
-              <div id="logo">
-                <img src="images/logo.png" usemap="#Map">
-                <map name="Map">
-                  <area shape="rect" coords="36,5,357,75" href="index.html">
-                  <area shape="rect" coords="674,3,762,73" href="http://www.conabio.gob.mx" target="_blank">
-                </map>
-              </div>
-				
-				<!-- Nav -->
-					<nav id="nav">
-						<ul>
-							<li ><a href="index.php">Inicio</a></li>
-							<li><a href="qs_dk.php">¿Quién es DK?</a></li>
-							<li><a href="quinta.php">Quinta</a></li>
-							<li><a href="proyecto.php">Proyecto</a></li>
-							<li class="current"><a href="recursos.php">Recursos</a></li>
-							<li>
-								<?php include('menu.php') ?>							
-						   	</li>
-						</ul>
-			  		</nav>
-
-		</div>
 			
 		<!-- Main -->
 			<section class="wrapper style1">
 				<div class="container">
-	                <p>&nbsp;</p>	
+					<p>&nbsp;</p>	
 	                <p>&nbsp;</p>	
 	                <header>
 							<h3><?php echo "Receta - ".$nombrereceta; ?></h3>
 					</header>
-        
- 	               	<div>
+         	               	
  	               	<p>&nbsp;</p>	
     				<p>&nbsp;</p>	
-	               	<?php
-						// PDF resultado
-						///echo "<embed src='".$URL."' style='position:relative;top:10px;bottom:0px;'>";
-						//echo "<embed width='100' height='100' src='".$URL."' frameborder='0'></embed>";
-						echo "<object data='".$URL."' type='application/pdf' width='100' height='100'>";
-  						echo "<p>It appears you don't have a PDF plugin for this browser. You can <a href='".$URL."'>click here to download the PDF file.</a></p>";
-  						echo "</object>";
-					?>
+    				<div class='micaja2'>
+	               		<div class='izq2'>
+	               			<div class='micontenido2'>
+			               	<?php
+								// PDF resultado
+								//echo "<embed src='".$URL."' style='position:relative;top:10px;bottom:0px;'>";
+								//echo "<embed width='100' height='100' src='".$URL."' frameborder='0'></embed>";
+								echo "<object data='".$URL."' type='application/pdf' width='100' height='100'>";
+		  						echo "<p>Usted no tiene instalado el plugin. Puede descargar la receta en formato PDF en <a href='".$URL."'>.</a></p>";
+		  						echo "</object>";
+							?>
+							</div>	
+						</div>
+						<div class='der2'>
+							<div class='micontenido2'>
+							<?php								
+								$consultarec=mysql_query("SELECT URL, recetanombre, publico FROM diannakennedy WHERE ".$filtro." AND recetanombre <> '".$nombrereceta."' ORDER BY recetanombre LIMIT 5", $conexion);								
+								$i = 1;
+								$j = 1;
+								while($registros=mysql_fetch_array($consultarec))
+								{									
+									$j += 1;
+									if ($j <= 5)																		
+										echo "<a href='muestrareceta.php?urlreceta=".$registros[0]."&nombrereceta=".htmlentities($registros[1])."&filtro=".$filtro."'><img src='images/thumb".$i.".jpg' width='36' height='36' alt='' />&nbsp;&nbsp;&nbsp;".htmlentities($registros[1])."</a><br><br>";
+									$i += 1;
+								    if ($i == 4)
+								    	$i = 1;
+								}
+							?>	
+							</div>						
+						</div>
 					</div>
-					
-				</div>
-
+				</div>               
 			</section>			
 
-		<!-- Footer -->
-		<div id="footer">
-        
-		<!-- Copyright -->
-					<div class="copyright">
-					&copy; 2014 Comisión Nacional para el Conocimiento y Uso de la Biodiversidad (CONABIO)
-                    </div>        
-        
-        <!-- Icons -->
-					<ul class="icons">
-						<li><a href="http://www.facebook.com/pages/Comision-Nacional-para-el-Conocimiento-y-uso-de-la-Biodiversidad/208239404119?v=wall" class="icon fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
-                      <li><a href="http://twitter.com/conabio" class="icon fa-twitter" target="_blank"><span class="label">Twitter</span></a></li>
-						<li><a href="http://www.youtube.com/biodiversidadmexico" class="icon fa-youtube" target="_blank"><span class="label">Youtube</span></a></li>
-						<li><a href="https://soundcloud.com/conabio" class="icon fa-soundcloud" target="_blank"><span class="label">SoundCloud</span></a></li>
-					</ul>
+		<?php include('footer.php') ?>
 
-		
-			</div>
-
-	</body>
-</html>
