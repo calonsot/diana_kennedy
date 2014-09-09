bb2be1eb3219424c477eec87ca7b537aaa4318a0
Carlos Alonso Torres <calonso@calonso-debian>
2014-09-09 12:45:02 -0500

    poniendo de nuevo ganalytics

--- footer.php
+++ footer.php
@@ -16,16 +16,7 @@
 
 		
 			</div>
-<!-- Google Analytics -->
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("UA-8226401-1");
-pageTracker._trackPageview();
-} catch(err) {}</script>
-<!-- Fin Google Analytics -->
+			<script src="http://bdi.conabio.gob.mx/fotoweb/googleAnalytics.js"></script>
+			<script src="js/gAnalytics.js"></script>
 	</body>
 </html>
