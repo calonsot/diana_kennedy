a73bb21b28cb363aaca0fdd69e107dbd1290ef9d
Carlos Alonso Torres <calonso@calonso-debian>
2014-09-09 14:39:53 -0500

    quitando lineas de prueba

--- menu.php
+++ menu.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//ini_set('display_errors', 1);
 header( 'Content-type: text/html; charset=utf-8' );
 require('config.php');
 
