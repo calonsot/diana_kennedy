23de3d542f49547db948e8f754ee6d0dba74a6a3
iiibella <israel.martinez@gmail.com>
2014-12-08 17:23:07 -0600

    Menu en IE que no se oculte
    El menu downdrop que se utiliza para las recetas se oculta en Internet
    Explorer, este cambio corrige y se reestructura el como se muestra el
    pdf.

--- muestrarecetas.php
+++ muestrarecetas.php
@@ -3,6 +3,15 @@ include('menu.php');
 $URL = $_GET["urlreceta"];
 $nombrereceta = $_GET["nombrereceta"];	
 $filtro =  isset($_GET["filtro"])? $_GET["filtro"]: "" ;
+$flag=0;
+
+$navigator_user_agent = (isset($_SERVER['HTTP_USER_AGENT'])) ? strtolower($_SERVER['HTTP_USER_AGENT']):'';
+if(stristr($navigator_user_agent, "trident")){
+$URL = utf8_encode($URL);
+$nombrereceta = utf8_encode($nombrereceta);	
+$filtro = utf8_encode($filtro);
+$flag=1;
+}
 ?>
 			
 		<!-- Main -->
@@ -18,18 +27,24 @@ $filtro =  isset($_GET["filtro"])? $_GET["filtro"]: "" ;
 								// PDF resultado
 								//echo "<embed src='".$URL."' style='position:relative;top:10px;bottom:0px;'>";
 								//echo "<embed width='100' height='100' src='".$URL."' frameborder='0'></embed>";
-			               		if (!empty($URL))
+			               		if($flag==0){
+								if (!empty($URL))
 			               		{
 				               		echo "<h3>Receta - ".$nombrereceta."<h3>";
 				               	    echo "<p>&nbsp;</p>";
 									echo "<object data='".$URL."' type='application/pdf' width='100' height='100'>";
-			  						echo "<p>Usted no tiene instalado el plugin. Puede descargar la receta en formato PDF <a href='".$URL."'>".$nombrereceta.".</a></p>";
+			  						echo "<p>Usted no tiene instalado el plugin. Puede descargar la receta en formato PDF en <a href='".$URL."'>.</a></p>";
 			  						echo "</object>";
 		  						}
 		  						else
 		  						{
 		  							echo "<h3>Receta - ".$nombrereceta." | Próximamente<h3>";
-		  						}
+		  						}}
+								else{
+									echo "<h3>Receta - ".$nombrereceta."<h3>";
+				               	    echo "<p>&nbsp;</p>";
+									echo "<iframe src='http://localhost/dianak/LibreriaPDF/web/viewer.php?url=".$URL."' width='678' height='678' frameborder=0 scrolling='no'></iframe>";
+								}
 							?>
 							</div>	
 						</div>			
@@ -43,7 +58,7 @@ $filtro =  isset($_GET["filtro"])? $_GET["filtro"]: "" ;
 									echo "<br><p><b>Ingredientes</b></p>";									
 									$lista=explode(",", $listaingrediente);
 									foreach ($lista as $valor) {
- 									   echo "<a target='_blank' href='http://bdi.conabio.gob.mx/fotoweb/Grid.fwx?archiveId=5062&columns=4&rows=8&search=".trim($valor)."'>".$valor."</a>.";
+ 									   echo "<a target='_blank' href='http://bdi.conabio.gob.mx/fotoweb/Grid.fwx?archiveId=5062&columns=4&rows=8&search=".trim($valor)."'>".$valor."</a><br>";
 									}
 								}
 							?>	
